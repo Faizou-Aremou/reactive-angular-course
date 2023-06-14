2ca0dec29e108ab6019f1f2d16513d662fc24cc5
Faïzou Aremou <faizouaremou@gmail.com>
2023-06-14 14:08:43 +0200

    finish message service

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { filter } from "rxjs/operators";
 
 @Injectable()
 export class MessagesService {
-
-  showErrors(...errors:string[]){
-
+  private subject = new BehaviorSubject<string[]>([]);
+  errors$: Observable<string[]> = this.subject.asObservable().pipe(
+    filter((errors)=> errors.length > 0),
+  );
+  showErrors(...errors: string[]) {
+    this.subject.next(errors);
   }
 }
