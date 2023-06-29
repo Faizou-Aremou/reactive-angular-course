7f85175038223de58d61e45f19823a4938f7c85c
Faizou <faizouaremou@gmail.com>
2023-06-29 08:27:23 +0200

    state management for loading data

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
+import { catchError, map, shareReplay } from "rxjs/operators";
 import { Course } from "../model/course";
 
 @Injectable({ providedIn: 'root' })
--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,15 +1,23 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
 import { Course, sortCoursesBySeqNo } from "../model/course";
-import { map } from "rxjs/operators";
+import { catchError, map, tap } from "rxjs/operators";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
+import { CoursesService } from "./courses.service";
 
 @Injectable({
   providedIn: 'root'
 })
 //service statefull, service facade, viewModel, modelClass
 export class CoursesStore {
-  courses$: Observable<Course[]>;
+  private subject = new BehaviorSubject<Course[]>([]);
 
+  courses$: Observable<Course[]> = this.subject.asObservable();
+
+  constructor(private coursesService: CoursesService, private loading: LoadingService, private messages: MessagesService) {
+    this.loadAllCourses();
+  }
 
   // you can have only one way to be notified by new data. 
   // thus this function can return instead void and use subject to provide courses data. 
@@ -18,6 +26,19 @@ export class CoursesStore {
       map(courses => courses.filter(course => course.category === category).sort(sortCoursesBySeqNo)
       )
     )
+  }
+
+  private loadAllCourses() {
+    const loadCourses$ = this.coursesService.loadAllCourses().pipe(
+      catchError(err => {
+        const message = "Could not not load courses";
+        this.messages.showErrors(message);
+        console.error(message, err);
+        return throwError(err);
+      }),
+      tap(courses => this.subject.next(courses))
+    );
+    this.loading.showLoaderUntilCompleted(loadCourses$).subscribe();
   }
 
 }
